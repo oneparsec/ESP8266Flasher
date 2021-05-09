0ca60b263812a6183f4fcd8fd1f0384ed5cd0511
Alexandr Simonov <asimonov1998@gmail.com>
2021-05-09 20:56:19 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from PyQt5.QtCore import QIODevice
 
 openpath = "C:\\"
 fileName = 0
-version = "0.1.0beta"
+speeds = ["9600", "115200"]
+# \\index        0        1       2      3      4      5      6
+flashsizes = ["256KB", "512KB", "1MB", "2MB", "4MB", "8MB", "16MB"]
+flashmodes = ["qio", "qout", "dio", "dout"]
+version = "0.2.0"
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("design.ui")
 ui.setWindowTitle("ESP8266 Flasher")
@@ -32,18 +36,33 @@ def flash():
 		ui.statusbar.showMessage("Error! Please, first open a BIN file")
 	else:
 		COM = ui.comlist.currentText()
-		command = "esptool.exe -p " + COM + " -b 115200" + " write_flash 0 " + fileName[0]
+		speed = ui.speedlist.currentText()
+		flash = ui.flashlist.currentText()
+		flashmode = ui.modelist.currentText()
+		command = "esptool.exe -p " + COM + " -b " + speed + " write_flash " + "--flash_mode " + flashmode + " --flash_size " + flash + " 0x0 " +fileName[0]
+		print(command)
 		code = os.system(command)
-		print(code)
 		if code == -1 or code == 2 or code == 1:
 			ui.statusbar.showMessage("Flash not sucess. Maybe you choose wrong port or BIN file?")
 		else:
-			ui.statusbar.showMessage("Flash sucess. Thanks for using ESP8266 Flasher")	
-
+			ui.statusbar.showMessage("Flash sucess. Thanks for using ESP8266 Flasher")
+def erase_flash():
+	COM = ui.comlist.currentText()
+	ui.statusbar.showMessage("Start erasing")
+	erase_command = "esptool.exe -p "+ COM + " erase_flash"
+	print(erase_command)
+	os.system(erase_command)
 
 ui.openF.clicked.connect(openfile)
 ui.flashB.clicked.connect(flash)
 ui.comlist.addItems(portList)
+ui.speedlist.addItems(speeds)
+ui.speedlist.setCurrentIndex(1)
+ui.flashlist.addItems(flashsizes)
+ui.flashlist.setCurrentIndex(2)
+ui.modelist.addItems(flashmodes)
+ui.modelist.setCurrentIndex(2)
+ui.erase.clicked.connect(erase_flash)
 
 ui.show()
 app.exec()

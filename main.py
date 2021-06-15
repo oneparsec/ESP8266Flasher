064f940aac0164d4ad5a3cd1651e21479fce7af4
Alexandr Simonov <asimonov1998@gmail.com>
2021-06-15 15:01:07 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,74 +1,87 @@
 import os
 import sys
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QInputDialog, QApplication, QMainWindow, QTextEdit, QAction, QFileDialog, QApplication
-from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
-from PyQt5.QtCore import QIODevice
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from os.path import expanduser
+from PyQt5.QtSerialPort import QSerialPortInfo
+from PyQt5.QtGui import QIcon
+from design import Ui_MainWindow
 
 if sys.platform == "win32":
 	esptoolName = "esptool.exe"
 else:
 	esptoolName = "./esptool"
-
-openpath = "C:\\"
+home = expanduser("~")
+openpath = home
 fileName = 0
-speeds = ["9600", "115200"]
-# \\index        0        1       2      3      4      5      6
+speeds = ["9600", "19200", "38400", "57600", "74880", "115200"]
+#   index        0        1       2      3      4      5      6
 flashsizes = ["256KB", "512KB", "1MB", "2MB", "4MB", "8MB", "16MB"]
 flashmodes = ["qio", "qout", "dio", "dout"]
-version = "0.2.0"
-app = QtWidgets.QApplication([])
-ui = uic.loadUi("design.ui")
-ui.setWindowTitle("ESP8266 Flasher")
-ui.statusbar.showMessage("Version " + version)	
+version = "0.2.1"
 
-
-portList = []
-ports = QSerialPortInfo().availablePorts()
-
-for port in ports:
-	portList.append(port.portName())
-
-def openfile():
-	global fileName
-	fileName = QFileDialog.getOpenFileName(ui, "Open BIN File", openpath , "*.bin")
-	if fileName[0] != '':
-		ui.statusbar.showMessage("BIN file opened")
-		print(fileName)
-	else:
-		ui.statusbar.showMessage("BIN file not opened. Please, try again.")
-def flash():
-	if fileName == 0:
-		ui.statusbar.showMessage("Error! Please, first open a BIN file")
-	else:
-		COM = ui.comlist.currentText()
-		speed = ui.speedlist.currentText()
-		flash = ui.flashlist.currentText()
-		flashmode = ui.modelist.currentText()
-		command = esptoolName + " -p " + COM + " -b " + speed + " write_flash " + "--flash_mode " + flashmode + " --flash_size " + flash + " 0x0 " +fileName[0]
-		print(command)
-		code = os.system(command)
-		if code == -1 or code == 2 or code == 1:
-			ui.statusbar.showMessage("Flash not sucess. Maybe you choose wrong port or BIN file?")
+class MainWindow(QtWidgets.QMainWindow):
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+		self.ui.statusbar.showMessage("Version " + version)
+		self.ui.openF.clicked.connect(self.openfile)
+		self.ui.flashB.clicked.connect(self.flash)
+		self.ui.comlist.addItems(portList)
+		self.ui.speedlist.addItems(speeds)
+		self.ui.speedlist.setCurrentIndex(5)
+		self.ui.flashlist.addItems(flashsizes)
+		self.ui.flashlist.setCurrentIndex(2)
+		self.ui.modelist.addItems(flashmodes)
+		self.ui.modelist.setCurrentIndex(2)
+		self.ui.erase.clicked.connect(self.erase_flash)	
+	def openfile(self):
+		global fileName
+		fileName = QFileDialog.getOpenFileName(self, "Open BIN File", openpath , "*.bin")
+		if fileName[0] != '':
+			self.ui.statusbar.showMessage("BIN file opened")
+			print(fileName)
 		else:
-			ui.statusbar.showMessage("Flash sucess. Thanks for using ESP8266 Flasher")
-def erase_flash():
-	COM = ui.comlist.currentText()
-	ui.statusbar.showMessage("Start erasing")
-	erase_command = "esptool.exe -p "+ COM + " erase_flash"
-	print(erase_command)
-	os.system(erase_command)
+			self.ui.statusbar.showMessage("BIN file not opened. Please, try again.")
+	def flash(self):
+		if fileName == 0:
+			self.ui.statusbar.showMessage("Error! Please, first open a BIN file")
+		else:
+			COM = self.ui.comlist.currentText()
+			speed = self.ui.speedlist.currentText()
+			flash = self.ui.flashlist.currentText()
+			flashmode = self.ui.modelist.currentText()
+			command = esptoolName + " -p " + COM + " -b " + speed + " write_flash " + "--flash_mode " + flashmode + " --flash_size " + flash + " 0x0 " +fileName[0]
+			print(command)
+			code = os.system(command)
+			if code == -1 or code == 2 or code == 1:
+				self.ui.statusbar.showMessage("Flash not sucess. Maybe you choose wrong port or BIN file?")
+			else:
+				self.ui.statusbar.showMessage("Flash sucess. Thanks for using ESP8266 Flasher")
+	def erase_flash(self):
+		COM = self.ui.comlist.currentText()
+		self.ui.statusbar.showMessage("Start erasing")
+		erase_command = "esptool.exe -p "+ COM + " erase_flash"
+		print(erase_command)
+		os.system(erase_command)
+	def refresh(self):
+		ports = QSerialPortInfo().availablePorts()
+		for port in ports:
+			portList.append(port.portName())
 
-ui.openF.clicked.connect(openfile)
-ui.flashB.clicked.connect(flash)
-ui.comlist.addItems(portList)
-ui.speedlist.addItems(speeds)
-ui.speedlist.setCurrentIndex(1)
-ui.flashlist.addItems(flashsizes)
-ui.flashlist.setCurrentIndex(2)
-ui.modelist.addItems(flashmodes)
-ui.modelist.setCurrentIndex(2)
-ui.erase.clicked.connect(erase_flash)
 
-ui.show()
-app.exec()
+
+if __name__ == "__main__":
+	app = QtWidgets.QApplication([])
+	portList = []
+	ports = QSerialPortInfo().availablePorts()
+
+	for port in ports:
+		portList.append(port.portName())
+
+	application = MainWindow()
+
+	application.show()
+	application.setWindowTitle("ESP8266 Flasher")
+	app.exec()
